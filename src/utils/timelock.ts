32aed05755e1a97b8973af7c543e8ee22cc659a6
namesty <nestor09amesty@gmail.com>
2020-10-19 10:48:17 +0200

    Extracted network variable and default it to rinkeby

--- src/utils/timelock.ts
+++ src/utils/timelock.ts
@@ -72,7 +72,7 @@ export const addBeneficiaries = async (week: WeekType | undefined) => {
 };
 
 export const deployTimeLockingContract = async (week: WeekType) => {
-  const network = process.env.DEVELOPMENT === 'true'? 'rinkeby': 'mainnet'
+  const network = process.env.NETWORK || 'rinkeby';
   const provider = new HDWalletProvider(
     process.env.PRIVATE_KEY as string,
     `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`
